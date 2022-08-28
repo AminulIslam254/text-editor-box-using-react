e76129d569b5ba12082e1b2df0ee7865c6cd33ee
AminulIslam254 <lostvayne684@gmail.com>
2022-08-28 16:50:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Mod from './contains/Module1';
+import Text from "./contains/Textbox";
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Mod title="lol"/>
+      <div className="container">
+        <Text/>
+      </div>
+      
+    </>
+  );
+}
+
+export default App;
